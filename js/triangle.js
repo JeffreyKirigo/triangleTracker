0cdc7f27dbe70d337aa4ba3f045f7bac23f9cf81
jeffrey muriithi <jeffreyem99@gmail.com>
2019-06-03 11:10:45 +0300

    Linked HTML and JS input fields

---
+++ js/triangle.js
@@ -0,0 +1,24 @@
+function Answer(){
+  function add(sideA,sideB,sideC){
+  return sideA+sideB<=sideC || sideB+sideC<=sideA || sideA+sideC<=sideB;
+  }
+  var sideA=parseFloat(document.getElementById("A").value);
+  var sideB=parseFloat(document.getElementById("B").value);
+  var sideC=parseFloat(document.getElementById("C").value);
+  
+  if(add(sideA,sideB,sideC)){
+  alert("Not a triangle");
+  }
+  else if (sideA===sideB && sideC===sideA){
+  alert("Equilateral triangle");
+  }
+  else if (sideA===sideB || sideC===sideA){
+  alert("An Isosceles triangle");
+  }
+  else if (sideA!==sideB && sideC!==sideA){
+  alert("scalene");
+  }
+  else{
+  alert("input valid data");
+    }
+};
