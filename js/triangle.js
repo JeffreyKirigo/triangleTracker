ef8df4a3a4c85cc10db9b187ab1df514d4b5e2ed
jeffrey muriithi <jeffreyem99@gmail.com>
2019-06-04 16:52:28 +0300

    Not a triangle functional

--- js/triangle.js
+++ js/triangle.js
@@ -1,28 +1,25 @@
-function Answer(){
-  function add(sideA,sideB,sideC){
-  return sideA+sideB<=sideC || sideB+sideC<=sideA || sideA+sideC<=sideB;
+function Answer() {
+  var sideA = parseFloat(document.getElementById("A").value);
+  var sideB = parseFloat(document.getElementById("B").value);
+  var sideC = parseFloat(document.getElementById("C").value);
+  var arr = ['Equilateral triangle', 'An Isosceles triangle', 'scalene']
+
+  if (sideA + sideB > sideC && sideB + sideC > sideA && sideA + sideC > sideB) {
+    if (!sideA, !sideB, !sideC) {
+      alert("Heee input a value");
+    }
+    else if (sideA === sideB && sideC === sideA) {
+      alert(arr[0]);
+    }
+    else if (sideA === sideB || sideC === sideA || sideB === sideC) {
+      alert(arr[1]);
+    }
+    else if (sideA !== sideB && sideC !== sideA) {
+      alert(arr[2]);
+    }
+
   }
-  var sideA=parseFloat(document.getElementById("A").value);
-  var sideB=parseFloat(document.getElementById("B").value);
-  var sideC=parseFloat(document.getElementById("C").value);
-  var arr=['Equilateral triangle','An Isosceles triangle','scalene']
-    
-  if(!sideA,!sideB,!sideC){
-    alert("Heee input a value");
-  }
-  else if (sideA===sideB && sideC===sideA){
-  alert(arr[0]);
-  }
-  else if (sideA===sideB || sideC===sideA){
-  alert(arr[1]);
-  }
-  else if (sideA!==sideB && sideC!==sideA){
-  alert(arr[2]);
-  }
-  else if(!add()){
+  else {
     alert("Not a triangle");
   }
-  else{
-    alert("input valid data");
-    }
-  };
+};
