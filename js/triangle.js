49832e3984c06553f7eb393fc2587da9bbdd60aa
jeffrey muriithi <jeffreyem99@gmail.com>
2019-06-04 13:04:20 +0300

    Readme spacing

--- js/triangle.js
+++ js/triangle.js
@@ -5,11 +5,10 @@ function Answer(){
   var sideA=parseFloat(document.getElementById("A").value);
   var sideB=parseFloat(document.getElementById("B").value);
   var sideC=parseFloat(document.getElementById("C").value);
+  
+    
   if(!sideA,!sideB,!sideC){
     alert("Heee input a value");
-  }
-  else if(!add()){
-  alert("Not a triangle");
   }
   else if (sideA===sideB && sideC===sideA){
   alert("Equilateral triangle");
@@ -20,7 +19,10 @@ function Answer(){
   else if (sideA!==sideB && sideC!==sideA){
   alert("scalene");
   }
+  else if(!add()){
+    alert("Not a triangle");
+  }
   else{
-  alert("input valid data");
+    alert("input valid data");
     }
-};
+  };
